12c9e9ccd5d5e16842b03d45163445ff81664ec1
lsryl13578 <tkdfuf1008@gmail.com>
2026-05-11 00:29:53 +0900

    feat: [Week 03] Spring Boot 실습 코드 작성 및 추가

---
+++ hello-spring/src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -0,0 +1,40 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.aop.TimeTraceAop;
+import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JpaMemberRepository;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.apache.catalina.Manager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+    // @Bean
+    // public MemberRepository memberRepository() {
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        // return new JdbcMemberRepository(dataSource);
+        // return new JpaMemberRepository(em);
+
+    // }
+}
