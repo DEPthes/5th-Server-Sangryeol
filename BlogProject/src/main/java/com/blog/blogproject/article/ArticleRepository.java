28bfe1b736af9bcc6ce695d4b2bb83d9ff9bc327
lsryl13578 <tkdfuf1008@gmail.com>
2026-07-05 21:44:12 +0900

    docs: [Week 08] ArticleRepository 클래스 Page 및 Pageable 관련 주석문 추가 작성

--- BlogProject/src/main/java/com/blog/blogproject/article/ArticleRepository.java
+++ BlogProject/src/main/java/com/blog/blogproject/article/ArticleRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // 게시글 엔티티의 CRUD 기능을 제공하는 Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    // Pageable 정보를 이용하여 페이징이 적용된 게시글 목록을 조회
     Page<Article> findAll(Pageable pageable);
 }
