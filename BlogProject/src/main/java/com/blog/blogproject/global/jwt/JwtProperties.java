2770f7d2e94fb4b50107afa735121717463e3f05
lsryl13578 <tkdfuf1008@gmail.com>
2026-07-06 18:16:41 +0900

    docs: [Week 08] JwtProperties 클래스 관련 주석문 추가 작성

--- BlogProject/src/main/java/com/blog/blogproject/global/jwt/JwtProperties.java
+++ BlogProject/src/main/java/com/blog/blogproject/global/jwt/JwtProperties.java
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties("jwt") // 자바 클래스에 프로퍼티값을 가져와서 사용하는 어노테이션
 public class JwtProperties {
+
+    // JWT를 발급한 주체(발급자)를 나타내는 값(iss 클레임)
     private String issuer;
+
+    // JWT 서명(Signature) 생성 및 검증에 사용하는 비밀 키
     private String secretKey;
 }
